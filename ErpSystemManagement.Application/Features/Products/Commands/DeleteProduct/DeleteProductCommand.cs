45cdf6d05f81900a0bd963c81fedcfe4cbb7f6ee
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:18:37 +0300

    DeleteProduct created.

---
+++ ErpSystemManagement.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,8 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Commands.DeleteProduct
+{
+    public record DeleteProductCommand(
+        Guid Id) : IRequest<IDomainResult<string>>;
+}
---
+++ ErpSystemManagement.Application/Features/Products/Handlers/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Products.Commands.DeleteProduct;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Handlers.DeleteProduct;
+
+class DeleteProductCommandHandler(
+    IProductRepository productRepository,
+    IUnitOfWork unitOfWork
+    ) : IRequestHandler<DeleteProductCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        Product? product = await productRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+        if (product is null) return DomainResult<string>.Failed("Ürün kaydı bulunamadı");
+
+        productRepository.Delete(product);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult<string>.Success("Ürün başarıyla silindi");
+    }
+}
