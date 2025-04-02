05bcb6c883355df3873834343eb884dc38248c33
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:17:21 +0300

    CreateDepot created.

---
+++ ErpSystemManagement.Application/Features/Depots/Commands/CreateDepot/CreateDepotCommand.cs
@@ -0,0 +1,10 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Depots.Commands.CreateDepot;
+
+public record CreateDepotCommand(
+    string Name,
+    string City,
+    string Town,
+    string FullAddress) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Depots/Handlers/CreateDepot/CreateDepotCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Depots.Commands.CreateDepot;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Depots.Handlers.CreateDepot;
+
+class CreateDepotCommandHandler(
+    IDepotRepository depotRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<CreateDepotCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(CreateDepotCommand request, CancellationToken cancellationToken)
+    {
+        Depot depot = mapper.Map<Depot>(request);
+        depotRepository.Add(depot);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult<string>.Success("Depo kaydı başarıyla tamamlandı");
+    }
+}
