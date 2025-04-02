8858696cdcf961a20eaf38a80fb738490076dd10
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:19:00 +0300

    GetAllProducts created.

---
+++ ErpSystemManagement.Application/Features/Products/Handlers/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Products.Queries.GetAllProducts;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpSystemManagement.Application.Features.Products.Handlers.GetAllProducts;
+
+public class GetAllProductsQueryHandler(IProductRepository productRepository) : IRequestHandler<GetAllProductsQuery, IDomainResult<IEnumerable<Product>>>
+{
+    public async Task<IDomainResult<IEnumerable<Product>>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Product> products = await productRepository.GetAll().OrderBy(p => p.Name).ToListAsync(cancellationToken);
+        return DomainResult.Success(products);
+    }
+}
---
+++ ErpSystemManagement.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQuery.cs
@@ -0,0 +1,7 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Domain.Entities;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Queries.GetAllProducts;
+
+public record GetAllProductsQuery() : IRequest<IDomainResult<IEnumerable<Product>>>;
