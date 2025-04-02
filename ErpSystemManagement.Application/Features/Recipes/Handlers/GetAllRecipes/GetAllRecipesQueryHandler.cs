e687854fcfb95c7a8a248f368fdc9c2c8f17552b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:20:06 +0300

    GetAllRecipes created.

---
+++ ErpSystemManagement.Application/Features/Recipes/Handlers/GetAllRecipes/GetAllRecipesQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Recipes.Queries.GetAllRecipes;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpSystemManagement.Application.Features.Recipes.Handlers.GetAllRecipes;
+
+class GetAllRecipesQueryHandler(
+    IRecipeRepository recipeRepository) : IRequestHandler<GetAllRecipesQuery, IDomainResult<IEnumerable<Recipe>>>
+{
+    public async Task<IDomainResult<IEnumerable<Recipe>>> Handle(GetAllRecipesQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Recipe> recipes = await recipeRepository
+            .GetAll()
+            .Include(r => r.Product)
+            .OrderBy(r => r.Product!.Name)
+            .ToListAsync(cancellationToken); 
+        return DomainResult.Success(recipes);
+    }
+}
---
+++ ErpSystemManagement.Application/Features/Recipes/Queries/GetAllRecipes/GetAllRecipesQuery.cs
@@ -0,0 +1,7 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Domain.Entities;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Recipes.Queries.GetAllRecipes;
+
+public record GetAllRecipesQuery() : IRequest<IDomainResult<IEnumerable<Recipe>>>;
