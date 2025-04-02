01a9d9b8a88dd1b9a44b7fadc7d48dea75a03263
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:18:47 +0300

    UpdateProduct created.

---
+++ ErpSystemManagement.Application/Features/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Commands.UpdateProduct
+{
+    public record UpdateProductCommand(
+        Guid Id,
+        string Name,
+        int Type) : IRequest<IDomainResult<string>>;
+}
---
+++ ErpSystemManagement.Application/Features/Products/Handlers/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Products.Commands.UpdateProduct;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Handlers.UpdateProduct;
+
+class UpdateProductCommandHandler(
+    IProductRepository productRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<UpdateProductCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        Product? product = await productRepository.GetByExpressionWithTrackingAsync(p => p.Id == request.Id, cancellationToken);
+        if (product is null) return DomainResult<string>.Failed("Ürün kaydı bulunamadı");
+
+        mapper.Map(request, product);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult<string>.Success("Ürün başarıyla güncellendi");
+    }
+}
