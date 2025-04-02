18dd0340984f1d79a632b52128720cec09dcabf5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:25:03 +0300

    DI

---
+++ ErpSystemManagement.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ErpSystemManagement.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(assembly);
+
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssemblies(assembly);
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
