196b295c9aa3380d94f3728f3067f9a1eba4b9ea
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:19:52 +0300

    DeleteRecipe created.

---
+++ ErpSystemManagement.Application/Features/Recipes/Commands/DeleteRecipe/DeleteRecipeCommand.cs
@@ -0,0 +1,6 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Recipes.Commands.DeleteRecipe;
+
+public record DeleteRecipeCommand(Guid Id) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Recipes/Handlers/DeleteRecipe/DeleteRecipeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Recipes.Commands.DeleteRecipe;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Recipes.Handlers.DeleteRecipe
+{
+    class DeleteRecipeCommandHandler(
+        IRecipeRepository recipeRepository,
+        IUnitOfWork unitOfWork): IRequestHandler<DeleteRecipeCommand, IDomainResult<string>>
+    {
+        public async Task<IDomainResult<string>> Handle(DeleteRecipeCommand request, CancellationToken cancellationToken)
+        {
+            Recipe? recipe = await recipeRepository.GetByExpressionAsync(r => r.Id == request.Id, cancellationToken);
+            if (recipe is null) return DomainResult<string>.Failed("Sistemde böyle bir reçete bulunmamaktadır");
+
+            recipeRepository.Delete(recipe);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return DomainResult<string>.Success("Reçete başarıyla silindi");
+        }
+    }
+}
