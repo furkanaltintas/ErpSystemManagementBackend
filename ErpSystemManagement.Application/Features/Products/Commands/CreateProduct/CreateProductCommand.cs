00dbecaa9d1cea37f43943b25710c192229fedeb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:18:30 +0300

    CreateProduct created.

---
+++ ErpSystemManagement.Application/Features/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,8 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Commands.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    int Type) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Products/Handlers/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Products.Commands.CreateProduct;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Products.Handlers.CreateProduct
+{
+    class CreateProductCommandHandler(
+        IProductRepository productRepository,
+        IUnitOfWork unitOfWork,
+        IMapper mapper) : IRequestHandler<CreateProductCommand, IDomainResult<string>>
+    {
+        public async Task<IDomainResult<string>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            Boolean isNameExists = await productRepository.AnyAsync(p => p.Name == request.Name, cancellationToken);
+            if (isNameExists) return DomainResult<string>.Failed("Ürün adı daha önce kullanılmış");
+
+            Product product = mapper.Map<Product>(request);
+            await productRepository.AddAsync(product, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+            return DomainResult<string>.Success("Ürün kaydı başarıyla tamamlandı");
+        }
+    }
+}
