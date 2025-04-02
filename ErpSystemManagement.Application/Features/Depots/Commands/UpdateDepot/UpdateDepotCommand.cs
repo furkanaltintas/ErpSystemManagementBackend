94a50f4efd199fb357289b2f45ee0557b2fbd23b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:17:41 +0300

    UpdateDepot created.

---
+++ ErpSystemManagement.Application/Features/Depots/Commands/UpdateDepot/UpdateDepotCommand.cs
@@ -0,0 +1,11 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Depots.Commands.UpdateDepot;
+
+public record UpdateDepotCommand(
+    Guid Id,
+    string Name,
+    string City,
+    string Town,
+    string FullAddress) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Depots/Handlers/UpdateDepot/UpdateDepotCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Depots.Commands.UpdateDepot;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Depots.Handlers.UpdateDepot;
+
+class UpdateDepotCommandHandler(
+    IDepotRepository depotRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<UpdateDepotCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(UpdateDepotCommand request, CancellationToken cancellationToken)
+    {
+        Depot? depot = await depotRepository.GetByExpressionWithTrackingAsync(d => d.Id == request.Id, cancellationToken);
+        if (depot == null) return DomainResult<string>.Failed("Depo kaydı bulunamadı");
+
+        mapper.Map(request, depot);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult<string>.Success("Depo başarıyla güncellendi");
+    }
+}
