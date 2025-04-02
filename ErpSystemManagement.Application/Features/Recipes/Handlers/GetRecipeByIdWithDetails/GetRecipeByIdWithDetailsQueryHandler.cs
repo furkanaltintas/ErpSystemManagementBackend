7f7a5d07f44e6d9ed652330887a5a2508ec69e14
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:20:40 +0300

    GetRecipeByIdWithDetails created.

---
+++ ErpSystemManagement.Application/Features/Recipes/Handlers/GetRecipeByIdWithDetails/GetRecipeByIdWithDetailsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Recipes.Queries.GetRecipeByIdWithDetails;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpSystemManagement.Application.Features.Recipes.Handlers.GetRecipeByIdWithDetails;
+
+class GetRecipeByIdWithDetailsQueryHandler(
+    IRecipeRepository recipeRepository) : IRequestHandler<GetRecipeByIdWithDetailsQuery, IDomainResult<Recipe>>
+{
+    public async Task<IDomainResult<Recipe>> Handle(GetRecipeByIdWithDetailsQuery request, CancellationToken cancellationToken)
+    {
+        Recipe? recipe = await recipeRepository
+            .Where(r => r.Id == request.RecipeId)
+            .Include(r => r.Product)
+            .Include(r => r.Details!.OrderBy(r => r.Product!.Name))
+            .ThenInclude(r => r.Product)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (recipe is null) return DomainResult<Recipe>.Failed("Ürüne ait reçete bulunamadı");
+
+        return DomainResult.Success(recipe);
+    }
+}
---
+++ ErpSystemManagement.Application/Features/Recipes/Queries/GetRecipeByIdWithDetails/GetRecipeByIdWithDetailsQuery.cs
@@ -0,0 +1,7 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Domain.Entities;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Recipes.Queries.GetRecipeByIdWithDetails;
+
+public record GetRecipeByIdWithDetailsQuery(Guid RecipeId) : IRequest<IDomainResult<Recipe>>;
