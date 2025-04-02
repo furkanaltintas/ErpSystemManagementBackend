ff1dba0005287c5a16865858321f0af26fc42f52
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:15:41 +0300

    UpdateCustomer created.

---
+++ ErpSystemManagement.Application/Features/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,13 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Commands.UpdateCustomer;
+
+public record UpdateCustomerCommand(
+    Guid Id,
+    string Name,
+    string TaxDepartment,
+    string TaxNumber,
+    string City,
+    string Town,
+    string FullAddress) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Customers/Handlers/UpdateCustomer/UpdateCustomerCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Customers.Commands.UpdateCustomer;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Handlers.UpdateCustomer;
+
+class UpdateCustomerCommandHandler(
+    ICustomerRepository customerRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<UpdateCustomerCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        Customer? customer = await customerRepository.GetByExpressionWithTrackingAsync(c => c.Id == request.Id, cancellationToken);
+        if (customer is null) return DomainResult<string>.Failed("Müşteri bulunamadı");
+
+        if(customer.TaxNumber != request.TaxNumber)
+        {
+            bool isTaxNumberExists = await customerRepository.AnyAsync(c => c.TaxNumber == request.TaxNumber);
+            if (isTaxNumberExists) return DomainResult<string>.Failed("Vergi numarası daha önce kaydedilmiş");
+        }
+
+        mapper.Map(request, customer);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult<string>.Success("Müşteri bilgileri başarıyla güncellendi");
+    }
+}
