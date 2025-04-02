dbc6d5d0f0cfdc527779822136415c292f2009b4
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:17:31 +0300

    DeleteDepot created.

---
+++ ErpSystemManagement.Application/Features/Depots/Commands/DeleteDepot/DeleteDepotCommand.cs
@@ -0,0 +1,6 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Depots.Commands.DeleteDepot;
+
+public record DeleteDepotCommand(Guid Id) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Depots/Handlers/DeleteDepot/DeleteDepotCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Depots.Commands.DeleteDepot;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Depots.Handlers.DeleteDepot;
+
+class DeleteDepotCommandHandler(
+    IDepotRepository depotRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteDepotCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(DeleteDepotCommand request, CancellationToken cancellationToken)
+    {
+        Depot? depot = await depotRepository.GetByExpressionAsync(d => d.Id == request.Id, cancellationToken);
+        if (depot is null) return DomainResult<string>.Failed("Depo bulunamadı");
+
+        depotRepository.Delete(depot);
+        await unitOfWork.SaveChangesAsync();
+        return DomainResult<string>.Success("Depo başarıyla silindi");
+    }
+}
