697a35e57c0b3ba1f4c3b9a41c94cb66b27d80e6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:21:17 +0300

    CreateRecipeDetail created.

---
+++ ErpSystemManagement.Application/Features/RecipeDetails/Commands/CreateRecipeDetail/CreateRecipeDetailCommand.cs
@@ -0,0 +1,9 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.RecipeDetails.Commands.CreateRecipeDetail;
+
+public record CreateRecipeDetailCommand(
+    Guid RecipeId,
+    Guid ProductId,
+    decimal Quantity) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/RecipeDetails/Handlers/CreateRecipeDetail/CreateRecipeDetailCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.RecipeDetails.Commands.CreateRecipeDetail;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.RecipeDetails.Handlers.CreateRecipeDetail;
+
+class CreateRecipeDetailCommandHandler(
+    IRecipeDetailRepository recipeDetailRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<CreateRecipeDetailCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(CreateRecipeDetailCommand request, CancellationToken cancellationToken)
+    {
+        RecipeDetail? recipeDetail = await recipeDetailRepository
+            .GetByExpressionWithTrackingAsync(
+            r => r.RecipeId == request.RecipeId && 
+            r.ProductId == request.ProductId);
+
+        if (recipeDetail is not null)
+        {
+            recipeDetail.Quantity += request.Quantity;
+        }
+        else
+        {
+            recipeDetail = mapper.Map<RecipeDetail>(request);
+            await recipeDetailRepository.AddAsync(recipeDetail, cancellationToken);
+        }
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult.Success("Reçete ürün kaydı başarıyla tamamlandı");
+
+    }
+}
+
