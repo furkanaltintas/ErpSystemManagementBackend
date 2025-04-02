1171786156b114c32ad970c1e0fd545eed9e980d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:15:28 +0300

    DeleteCustomer created.

---
+++ ErpSystemManagement.Application/Features/Customers/Commands/DeleteCustomer/DeleteCustomerCommand.cs
@@ -0,0 +1,6 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Commands.DeleteCustomer;
+
+public record DeleteCustomerCommand(Guid Id) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Customers/Handlers/DeleteCustomer/DeleteCustomerCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Customers.Commands.DeleteCustomer;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Handlers.DeleteCustomer;
+
+class DeleteCustomerCommandHandler(
+    ICustomerRepository customerRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteCustomerCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+    {
+        Customer? customer = await customerRepository.GetByExpressionAsync(c => c.Id == request.Id, cancellationToken);
+        if (customer is null) return DomainResult<string>.Failed("Müşteri bulunamadı");
+
+        customerRepository.Delete(customer);
+        await unitOfWork.SaveChangesAsync();
+        return DomainResult<string>.Success("Müşteri başarıyla silindi");
+    }
+}
