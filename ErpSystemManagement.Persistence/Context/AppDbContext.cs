56669cad3feb376279c1d9794ef0675c6320130b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:13:52 +0300

    Context created.

---
+++ ErpSystemManagement.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Persistence.Configurations;
+using GenericRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpSystemManagement.Persistence.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>, IUnitOfWork
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Depot> Depots { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipeDetail> RecipeDetails { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyCustomConfigurations();
+        }
+    }
+}
---
+++ ErpSystemManagement.Persistence/Context/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ErpSystemManagement.Persistence.Context;
+
+//public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+//{
+//    public AppDbContext CreateDbContext(string[] args)
+//    {
+//        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+//        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ErpSystemManagementDb;Integrated Security=True;");
+
+//        return new AppDbContext(optionsBuilder.Options);
+//    }
+//}
