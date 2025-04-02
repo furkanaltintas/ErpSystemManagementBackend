df3553ef37a5e085141714b487477d8961f5383a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:17:56 +0300

    GetAllDepots created.

---
+++ ErpSystemManagement.Application/Features/Depots/Handlers/GetAllDepots/GetAllDepotsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Depots.Queries.GetAllDepots;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpSystemManagement.Application.Features.Depots.Handlers.GetAllDepots;
+
+class GetAllDepotsQueryHandler(
+    IDepotRepository depotRepository) : IRequestHandler<GetAllDepotsQuery, IDomainResult<IEnumerable<Depot>>>
+{
+    public async Task<IDomainResult<IEnumerable<Depot>>> Handle(GetAllDepotsQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Depot> depots = await depotRepository.GetAll().OrderBy(d => d.Name).ToListAsync();
+        return DomainResult.Success(depots);
+    }
+}
