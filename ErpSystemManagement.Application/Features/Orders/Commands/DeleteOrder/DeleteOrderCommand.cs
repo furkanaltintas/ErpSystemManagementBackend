b4cdde7a4f213edafc9c90e796e5944c0b71dbff
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:23:08 +0300

    DeleteOrder created.

---
+++ ErpSystemManagement.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,5 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Orders.Commands.DeleteOrder;
+public record DeleteOrderCommand(Guid Id) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Orders/Handlers/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Orders.Commands.DeleteOrder;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Orders.Handlers.DeleteOrder;
+
+class DeleteOrderCommandHandler(
+    IOrderRepository orderRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteOrderCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        Order? order = await orderRepository.GetByExpressionAsync(o => o.Id == request.Id, cancellationToken);
+        if (order is null) return DomainResult<string>.Failed("Sipariş bulunamadı");
+
+        orderRepository.Delete(order);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult.Success("Sipariş başarıyla silindi");
+    }
+}
