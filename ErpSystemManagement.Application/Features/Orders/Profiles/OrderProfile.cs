f653bba507b8cf84b90e342c44ce8a227927fa34
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:24:45 +0300

    Orders Profile created.

---
+++ ErpSystemManagement.Application/Features/Orders/Profiles/OrderProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ErpSystemManagement.Application.Features.Orders.Commands.CreateOrder;
+using ErpSystemManagement.Application.Features.Orders.Commands.UpdateOrder;
+using ErpSystemManagement.Domain.Entities;
+
+namespace ErpSystemManagement.Application.Features.Orders.Profiles;
+
+class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<CreateOrderCommand, Order>()
+            .ForMember(member => member.Details, options => options.MapFrom(source => source.Details.Select(o => new OrderDetail
+            {
+                Price = o.Price,
+                ProductId = o.ProductId,
+                Quantity = o.Quantity
+            })));
+
+        CreateMap<UpdateOrderCommand, Order>()
+            .ForMember(member => member.Details, options => options.MapFrom(source => source.Details.Select(o => new OrderDetail
+            {
+                Price = o.Price,
+                ProductId = o.ProductId,
+                Quantity = o.Quantity
+            })));
+    }
+}
