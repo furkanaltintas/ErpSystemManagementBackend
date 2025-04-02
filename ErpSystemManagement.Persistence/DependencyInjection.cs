fb7df3adb75aa4a4c536286238272d4217c44d53
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:14:13 +0300

    DI

---
+++ ErpSystemManagement.Persistence/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using ErpSystemManagement.Persistence.Context;
+using ErpSystemManagement.Persistence.Repositories;
+using GenericRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ErpSystemManagement.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Local"));
+        });
+
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IDepotRepository, DepotRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IRecipeDetailRepository, RecipeDetailRepository>();
+        services.AddScoped<IRecipeRepository, RecipeRepository>();
+        services.AddScoped<IUnitOfWork>(srv => srv.GetRequiredService<AppDbContext>());
+
+        services.AddIdentity<AppUser, IdentityRole<Guid>>(cfr =>
+        {
+            cfr.Password.RequiredLength = 1;
+            cfr.Password.RequireNonAlphanumeric = false;
+            cfr.Password.RequireUppercase = false;
+            cfr.Password.RequireLowercase = false;
+            cfr.Password.RequireDigit = false;
+            cfr.SignIn.RequireConfirmedEmail = true;
+            cfr.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            cfr.Lockout.MaxFailedAccessAttempts = 3;
+            cfr.Lockout.AllowedForNewUsers = true;
+        })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+}
