1c5046998b20462327ba3258ef13c18474a606ae
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:18:10 +0300

    Depots Profile created.

---
+++ ErpSystemManagement.Application/Features/Depots/Profiles/DepotProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ErpSystemManagement.Application.Features.Depots.Commands.CreateDepot;
+using ErpSystemManagement.Application.Features.Depots.Commands.UpdateDepot;
+using ErpSystemManagement.Domain.Entities;
+
+namespace ErpSystemManagement.Application.Features.Depots.Profiles;
+
+public class DepotProfile : Profile
+{
+    public DepotProfile()
+    {
+        CreateMap<Depot, CreateDepotCommand>().ReverseMap();
+        CreateMap<Depot, UpdateDepotCommand>().ReverseMap();
+    }
+}
