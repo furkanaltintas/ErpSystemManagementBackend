7505d692c074378d9c37c06b03587f7fbea36989
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:15:55 +0300

    GetAllCustomers created.

---
+++ ErpSystemManagement.Application/Features/Customers/Handlers/GetAllCustomers/GetAllCustomersQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Customers.Queries.GetAllCustomers;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpSystemManagement.Application.Features.Customers.Handlers.GetAllCustomers;
+
+class GetAllCustomersQueryHandler(ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomersQuery, IDomainResult<IEnumerable<Customer>>>
+{
+    public async Task<IDomainResult<IEnumerable<Customer>>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Customer> customers = await customerRepository.GetAll().OrderBy(c => c.Name).ToListAsync();
+        return DomainResult.Success(customers);
+    }
+}
---
+++ ErpSystemManagement.Application/Features/Customers/Queries/GetAllCustomers/GetAllCustomersQuery.cs
@@ -0,0 +1,7 @@
+﻿using DomainResults.Common;
+using ErpSystemManagement.Domain.Entities;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Queries.GetAllCustomers;
+
+public record GetAllCustomersQuery() : IRequest<IDomainResult<IEnumerable<Customer>>>;
