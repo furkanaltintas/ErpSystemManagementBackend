20e8176e87601ed9ec572be43f5c4fa4d420d770
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-02 04:15:17 +0300

    CreateCustomer created.

---
+++ ErpSystemManagement.Application/Features/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,12 @@
+﻿using DomainResults.Common;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Commands.CreateCustomer;
+
+public record CreateCustomerCommand(
+    string Name,
+    string TaxDepartment,
+    string TaxNumber,
+    string City,
+    string Town,
+    string FullAddress) : IRequest<IDomainResult<string>>;
---
+++ ErpSystemManagement.Application/Features/Customers/Handlers/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DomainResults.Common;
+using ErpSystemManagement.Application.Features.Customers.Commands.CreateCustomer;
+using ErpSystemManagement.Domain.Entities;
+using ErpSystemManagement.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+
+namespace ErpSystemManagement.Application.Features.Customers.Handlers.CreateCustomer;
+
+class CreateCustomerCommandHandler(
+    ICustomerRepository customerRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRequestHandler<CreateCustomerCommand, IDomainResult<string>>
+{
+    public async Task<IDomainResult<string>> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        bool isTaxNumberExists = await customerRepository.AnyAsync(c => c.TaxNumber == request.TaxNumber);
+        if (isTaxNumberExists) return DomainResult<string>.Failed("Vergi numarası daha önce kaydedilmiş");
+
+        Customer customer = mapper.Map<Customer>(request);
+        customerRepository.Add(customer);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return DomainResult<string>.Success("Müşteri kaydı başarıyla tamamlandı");
+    }
+}
